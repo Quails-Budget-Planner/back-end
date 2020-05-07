d8de85481cc7329d4ef1825d36843e8d80a5e6af
Jonathan Ho <johnnyho82@gmail.com>
2020-05-07 19:05:10 -0400

    Updatea budgets router

--- api/budgets/budgets-router.js
+++ api/budgets/budgets-router.js
@@ -80,7 +80,18 @@ router.post('/', (req, res) => {
 });
 
 // PUT /api/budgets endpoint
-router.put('/:id', (req, res) => {});
+router.put('/:id', (req, res) => {
+	docClient.update(params, function (err, data) {
+		if (err) {
+			console.error(
+				'Unable to update item. Error JSON:',
+				JSON.stringify(err, null, 2),
+			);
+		} else {
+			console.log('UpdateItem succeeded:', JSON.stringify(data, null, 2));
+		}
+	});
+});
 
 // DELETE /api/budgets endpoint
 router.delete('/:id', (req, res) => {});
