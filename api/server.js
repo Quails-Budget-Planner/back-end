57ba3032c8d37ea569e56053a6272076f8ccd2c9
Jordan <jordandoan@hotmail.com>
2020-05-07 15:59:45 -0700

    added helmet and cors

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const AWS = require("aws-sdk");
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
 const server = express();
 const authRoutes = require("./auth/auth-router");
 
+server.use(helmet());
+server.use(cors());
 server.use(express.json());
+
 server.use("/api/auth", authRoutes);
 
 module.exports = server;
