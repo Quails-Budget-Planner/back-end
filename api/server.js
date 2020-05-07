f8dfd227b87999208666f1efd1664debf153e705
Jonathan Ho <johnnyho82@gmail.com>
2020-05-07 18:20:03 -0400

    Merge before pull

--- api/server.js
+++ api/server.js
@@ -1,34 +1,37 @@
-const AWS = require("aws-sdk");
-const express = require("express");
+const AWS = require('aws-sdk');
+const express = require('express');
+
 const server = express();
 
 AWS.config.update({
-  region:  "us-east-2",
-  endpoint: "https://dynamodb.us-east-2.amazonaws.com"
+	region: 'us-east-2',
+	endpoint: 'https://dynamodb.us-east-2.amazonaws.com',
 });
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-const table = "Users";
+const table = 'Users';
 
 const params = {
-    TableName: table,
-    Key: {
-      username: "jordandoan"
-    }
+	TableName: table,
+	Key: {
+		username: 'jordandoan',
+	},
 };
 
-server.get("/", (req,res) => {
-    docClient.get(params, (err,data) => {
-      if (err) {
-          console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-      } else {
-          // console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-          console.log(data);
-          res.json(data);
-      }
-  });
+server.get('/', (_req, res) => {
+	docClient.get(params, (err, data) => {
+		if (err) {
+			console.error(
+				'Unable to read item. Error JSON:',
+				JSON.stringify(err, null, 2),
+			);
+		} else {
+			// console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+			// console.log(data);
+			res.json(data);
+		}
+	});
 });
-
 
 module.exports = server;
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-require("dotenv").config();
+require('dotenv').config();
+
 const server = require('./api/server');
+
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  // tslint:disable-next-line:no-console
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+	console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
