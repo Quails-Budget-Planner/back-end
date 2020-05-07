7332524664b23833b254976017ceb577bca8f8e5
Jordan <jordandoan@hotmail.com>
2020-05-07 16:20:41 -0700

    Authorization done

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,7 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const docClient = require("../utils");
+
 module.exports = {
   validateRegister: (req, res, next) => {
     const { username, password, name } = req.body;
@@ -7,5 +11,44 @@ module.exports = {
       req.register = { username, password, name }
       next();
     }
+  },
+
+  validateLogin: (req, res, next) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      res.status(400).json({message: "Invalid body."});
+    } else {
+      req.login = { username, password };
+      next();
+    }
+  },
+
+  verifyPassword: (req, res, next) => {
+    const params = {
+      TableName: "Users",
+      Key: {
+        username: req.login.username
+      }
+    };
+
+    docClient.get(params, (err,data) => {
+      if (err) {
+        res.status(404).json({message: "Incorrect Credentials"});
+      } else {
+        if (bcrypt.compareSync(req.login.password, data.Item.password)) {
+          next();
+        } else {
+          res.status(404).json({message: "Incorrect Credentials"});
+        }
+      }
+    });
+  },
+  
+  createToken: (username) => {
+    const payload = {
+      username
+    };
+    const secret = process.env.JWT_SECRET || "HELLOOOO";
+    return jwt.sign(payload, secret); // this method is synchronous
   }
 }
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,33 +1,39 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { validateRegister } = require("./auth-middleware");
+const { validateRegister, validateLogin, verifyPassword, createToken } = require("./auth-middleware");
 const docClient = require("../utils");
 
 const router = express.Router();
 
 router.post("/register", validateRegister, (req, res) => {
   req.register.password = bcrypt.hashSync(req.register.password, 12);
+
   const params = {
     TableName: "Users",
     Item: {
       ...req.register
     },
+    // Checks for nonexisting username before inserting
     Expected: {
       'username': {
         Exists: false
       }
     }
   };
+
   docClient.put(params, function(err, data) {
-    console.log(err, data);
     if (err) {
       console.log("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-      res.status(500).json(err);
+      res.status(500).json({message: "Please provide a different username"});
     } else {
-      res.status(201).json({message: "Successful signup!", success: true});
+      res.status(201).json({token: createToken(req.register.username)});
     }
   });
+});
+
+router.post("/login", [validateLogin, verifyPassword], (req, res) => {
+  res.status(201).json({token: createToken(req.login.username)});
 });
 
 module.exports = router;
